1706d038498b1504173cb6bcf7c49d618636bed0
Andrei Cozari <andrei.cozari@windeln.ro>
2019-03-08 18:48:35 +0200

    Implement month converter. Refactor services.

--- src/main/java/com/energy/service/ConverterService.java
+++ src/main/java/com/energy/service/ConverterService.java
@@ -3,37 +3,44 @@ package com.energy.service;
 import org.springframework.stereotype.Service;
 import org.apache.commons.lang.StringUtils;
 
+import javax.annotation.PostConstruct;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.energy.utils.StringUtils.*;
 
 @Service
 public class ConverterService {
 
-    public String convertYear(String year) {
-        return year;
+    private  Map<String, String> monthsMapping;
+
+    public String convertYear(String period) {
+        return period;
     }
 
-    public String convertQuarter(String quarter) {
+    public String convertQuarter(String period) {
         StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder
-                .append(quarter.charAt(0))
-                .append(quarter.charAt(7))
+                .append(period.charAt(0))
+                .append(period.charAt(7))
                 .append("-")
-                .append(quarter.charAt(4))
-                .append(quarter.charAt(5));
+                .append(period.charAt(4))
+                .append(period.charAt(5));
 
         return stringBuilder.toString();
     }
 
-    public String convertSeason(String season) {
-        if(StringUtils.isBlank(season) || season.length() != 11) {
-            throw new IllegalArgumentException("Season must not be empty");
+    public String convertSeason(String period) {
+        if(StringUtils.isBlank(period) || period.length() != 11) {
+            throw new IllegalArgumentException("Season period is not valid");
         }
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        String shortSeason = calculateShortSeason(season.substring(season.length() - 2));
-        String shortYear = season.substring(6, 8);
+        String shortSeason = calculateShortSeason(period.substring(period.length() - 2));
+        String shortYear = period.substring(6, 8);
 
         stringBuilder
                 .append(shortSeason)
@@ -43,10 +50,40 @@ public class ConverterService {
         return stringBuilder.toString();
     }
 
-    public String convertMonth(String month) {
-        return month;
+    public String convertMonth(String period) {
+
+        if(StringUtils.isBlank(period) || period.length() != 9) {
+            throw new IllegalArgumentException("Month period is not valid.");
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String year = period.substring(2, 6);
+        String shortMonth = period.substring(period.length() - 2, period.length());
+
+        stringBuilder
+                .append(monthsMapping.get(shortMonth))
+                .append("-")
+                .append(year);
+
+        return stringBuilder.toString();
     }
 
+    @PostConstruct
+    private void initMonthsMapping() {
+        this.monthsMapping = new HashMap<>();
 
+        monthsMapping.put("01", "Jan");
+        monthsMapping.put("02", "Feb");
+        monthsMapping.put("03", "Mar");
+        monthsMapping.put("04", "Apr");
+        monthsMapping.put("05", "May");
+        monthsMapping.put("06", "Jun");
+        monthsMapping.put("07", "Jul");
+        monthsMapping.put("08", "Aug");
+        monthsMapping.put("09", "Sep");
+        monthsMapping.put("10", "Oct");
+        monthsMapping.put("11", "Nov");
+        monthsMapping.put("12", "Dec");
+    }
 
 }
--- src/test/java/com/energy/service/ConverterServiceTest.java
+++ src/test/java/com/energy/service/ConverterServiceTest.java
@@ -4,13 +4,11 @@ import com.energy.EnergyConverterApplication;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import static org.assertj.core.api.Assertions.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -18,7 +16,6 @@ import static org.mockito.MockitoAnnotations.initMocks;
 @SpringBootTest(classes = EnergyConverterApplication.class)
 public class ConverterServiceTest {
 
-    //@Mock
     @Autowired
     private ConverterService converterService;
 
@@ -28,37 +25,32 @@ public class ConverterServiceTest {
     }
 
     @Test
-    public void whenYearConverterIsCalled_thenResultIsTheSameValue() {
+    public void testConvertYear() {
         String yearToTest = "2018";
-        //Mockito.when(converterService.convertYear(yearToTest)).thenReturn(yearToTest);
         String resultYear = converterService.convertYear(yearToTest);
         assertEquals(yearToTest, resultYear);
     }
 
     @Test
-    public void whenQuarterConverterIsCalled_thenResultIsCorrect() {
+    public void testConvertQuarter() {
         String quarterInputToTest = "Q-2018-2";
         String expectedResult = "Q2-18";
-       // Mockito.when(converterService.convertQuarter(quarterInputToTest)).thenReturn(expectedResult);
         assertThat(converterService.convertQuarter(quarterInputToTest)).isEqualTo(expectedResult);
     }
 
 
     @Test
-    public void whenSeasonConverterIsCalled_thenResultIsCorrect() {
+    public void testConvertSeason() {
         String seasonInputToTest = "SWS-2018-04";
         String expectedResult = "Spr-18";
-        //Mockito.when(converterService.convertSeason(seasonInputToTest)).thenReturn(expectedResult);
         String resultQuarter = converterService.convertSeason(seasonInputToTest);
         assertEquals(expectedResult, resultQuarter);
     }
 
-
     @Test
-    public void whenMonthConverterIsCalled_thenResultIsCorrect() {
+    public void testMonthConverter() {
         String monthInputToTest = "M-2018-02";
         String expectedResult = "Feb-2018";
-        //Mockito.when(converterService.convertMonth(monthInputToTest)).thenReturn(expectedResult);
         String resultQuarter = converterService.convertMonth(monthInputToTest);
         assertEquals(expectedResult, resultQuarter);
     }
