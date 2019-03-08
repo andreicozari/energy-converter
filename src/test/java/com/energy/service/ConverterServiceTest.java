2e53af8b3aa354e25514f8feeaec0c6224e6e801
Andrei Cozari <andrei.cozari@windeln.ro>
2019-03-08 17:52:37 +0200

    Refactor unit tests.

--- src/test/java/com/energy/service/ConverterServiceTest.java
+++ src/test/java/com/energy/service/ConverterServiceTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.assertj.core.api.Assertions.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -16,7 +18,8 @@ import static org.mockito.MockitoAnnotations.initMocks;
 @SpringBootTest(classes = EnergyConverterApplication.class)
 public class ConverterServiceTest {
 
-    @Mock
+    //@Mock
+    @Autowired
     private ConverterService converterService;
 
     @Before
@@ -27,7 +30,7 @@ public class ConverterServiceTest {
     @Test
     public void whenYearConverterIsCalled_thenResultIsTheSameValue() {
         String yearToTest = "2018";
-        Mockito.when(converterService.convertYear(yearToTest)).thenReturn(yearToTest);
+        //Mockito.when(converterService.convertYear(yearToTest)).thenReturn(yearToTest);
         String resultYear = converterService.convertYear(yearToTest);
         assertEquals(yearToTest, resultYear);
     }
@@ -36,9 +39,8 @@ public class ConverterServiceTest {
     public void whenQuarterConverterIsCalled_thenResultIsCorrect() {
         String quarterInputToTest = "Q-2018-2";
         String expectedResult = "Q2-18";
-        Mockito.when(converterService.convertQuarter(quarterInputToTest)).thenReturn(expectedResult);
-        String resultQuarter = converterService.convertQuarter(quarterInputToTest);
-        assertEquals(expectedResult, resultQuarter);
+       // Mockito.when(converterService.convertQuarter(quarterInputToTest)).thenReturn(expectedResult);
+        assertThat(converterService.convertQuarter(quarterInputToTest)).isEqualTo(expectedResult);
     }
 
 
@@ -46,7 +48,7 @@ public class ConverterServiceTest {
     public void whenSeasonConverterIsCalled_thenResultIsCorrect() {
         String seasonInputToTest = "SWS-2018-04";
         String expectedResult = "Spr-18";
-        Mockito.when(converterService.convertSeason(seasonInputToTest)).thenReturn(expectedResult);
+        //Mockito.when(converterService.convertSeason(seasonInputToTest)).thenReturn(expectedResult);
         String resultQuarter = converterService.convertSeason(seasonInputToTest);
         assertEquals(expectedResult, resultQuarter);
     }
@@ -56,7 +58,7 @@ public class ConverterServiceTest {
     public void whenMonthConverterIsCalled_thenResultIsCorrect() {
         String monthInputToTest = "M-2018-02";
         String expectedResult = "Feb-2018";
-        Mockito.when(converterService.convertMonth(monthInputToTest)).thenReturn(expectedResult);
+        //Mockito.when(converterService.convertMonth(monthInputToTest)).thenReturn(expectedResult);
         String resultQuarter = converterService.convertMonth(monthInputToTest);
         assertEquals(expectedResult, resultQuarter);
     }
